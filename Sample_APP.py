2419b405494b1a3962f0aab9b170489878c6d17b
joelfern <joelfern@cisco.com>
2017-02-21 13:13:45 +0800

    Update 2

--- Sample_APP.py
+++ Sample_APP.py
@@ -1,3 +1,28 @@
+#
+#   Cisco Network Services Orchestrator(NSO) Wrapper API
+#       v.01
+#
+#   Joel Fernandez(joelfern@cisco.com)
+#       Feb 2017
+#
+#       This class provides methods to facilitates
+#       access to the Network Services Orchestrator API.
+#
+#   REQUIREMENTS:
+#       Python sys library
+#       NSO Wrapper_API module to access the NSO API
+#
+#   WARNING:
+#       Any use of these scripts and tools is at
+#       your own risk. There is no guarantee that
+#       they have been through thorough testing in a
+#       comparable environment and we are not
+#       responsible for any damage or data loss
+#       incurred with their use.
+#
+#   INFORMATION:
+#       If you require any information please contact gve-programmability@cisco.com
+
 import sys
 from Wrapper_API import Wrapper_API
 
--- Wrapper_API.py
+++ Wrapper_API.py
@@ -1,3 +1,27 @@
+#
+#   Cisco Network Services Orchestrator(NSO) Wrapper API
+#        v.01
+#
+#   Joel Fernandez(joelfern@cisco.com)
+#       Feb 2017
+#
+#       This class provides methods to facilitates
+#       access to the Network Services Orchestrator API.
+#
+#   REQUIREMENTS:
+#       Python requests library (issue the "pip install requests" command in shell or cmd)
+#
+#   WARNING:
+#       Any use of these scripts and tools is at
+#       your own risk. There is no guarantee that
+#       they have been through thorough testing in a
+#       comparable environment and we are not
+#       responsible for any damage or data loss
+#       incurred with their use.
+#
+#   INFORMATION:
+#       If you require any information please contact gve-programmability@cisco.com
+
 import requests
 
 host = '198.18.134.28:8080'
